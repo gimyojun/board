8f994a5b7ac7f7de05ec0327549801f2917bbd35
gimyojun <gywnsdl5623@gmail.com>
2023-11-29 04:12:07 +0900

    html 헤드 설정 + msg ttl 설정

--- src/main/java/com/ll/board/global/rq/Rq.java
+++ src/main/java/com/ll/board/global/rq/Rq.java
@@ -15,6 +15,7 @@ import org.springframework.web.context.annotation.RequestScope;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 //기본적으로 빈은 어플리케이션 스코프지만 이런경우 명시적으로 리퀘스트 스코프다.
 @RequestScope
@@ -40,7 +41,7 @@ public class Rq {
 
     public String redirect(String path, String msg) {
         msg = URLEncoder.encode(msg, StandardCharsets.UTF_8);
-
+        msg += ";ttl=" + (new Date().getTime() + 1000 * 5);
         return "redirect:" + path + "?msg=" + msg;
     }
 
